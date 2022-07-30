86c357e8b89582501185d0471c8f54844cd408e0
John Doe <I.am.Nikisha@yandex.ru>
2022-07-30 21:25:11 +0300

    counter 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useRef, useState} from 'react';
 import './App.css';
 
+
 function App() {
+  let [counter, setCounter] = useState(0)
+
+  const incButtonHandler = () => {setCounter(counter + 1)}
+
+  const resetButtonHandler = () => {setCounter(0)}
+
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const set = () => {
+        const el = inputRef.current as HTMLInputElement
+        setCounter(+el.value)
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input type={"number"} ref={inputRef}/> max value
+        <button onClick={set}>SET</button>
+        <h3 className={counter === 5 ? "red" : "counter"} >{counter}</h3>
+        <Button inc={incButtonHandler} reset={resetButtonHandler} counter={counter}/>
     </div>
   );
 }
+
+
+type buttonType = {
+    inc:()=>void
+    reset:()=>void
+    counter: number
+
+}
+
+export const Button = (props: buttonType) => {
+    return <div className="buttonIncReset">
+        <button disabled={props.counter >= 5} onClick={props.inc}>INC</button>
+        <button onClick={props.reset}>RESET</button>
+    </div>
+}
+
+
+
+
+
+
+
 
 export default App;
