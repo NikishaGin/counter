f00dd3ceefa4bba1b0c2ae8b97d0dfae9bc567bb
John Doe <I.am.Nikisha@yandex.ru>
2022-08-04 23:07:54 +0300

    counter 2.0 set

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,32 @@ import './App.css';
 
 function App() {
   let [counter, setCounter] = useState(0)
+    const [minValue, setMinValue] = useState<number>(0)
+    const [maxValue, setMaxValue] = useState<number>(0)
+    const [message, setMessage] = useState<string>('')
 
   const incButtonHandler = () => {setCounter(counter + 1)}
 
   const resetButtonHandler = () => {setCounter(0)}
 
-
-    const inputRef = useRef<HTMLInputElement>(null)
-
     const set = () => {
-        const el = inputRef.current as HTMLInputElement
-        setCounter(+el.value)
+        setCounter(minValue)
+        setMessage('')
     }
-
+    const changeMinValue = (e: ChangeEvent<HTMLInputElement>) => {
+      setMinValue(+e.currentTarget.value)
+    }
+    const changeMaxValue = (e: ChangeEvent<HTMLInputElement>) => {
+      setMaxValue(+e.currentTarget.value)
+    }
 
   return (
     <div className="App">
-        <input type={"number"} ref={inputRef}/> max value
+        <input type={"number"} value={minValue} onChange={changeMinValue}/> min value
+        <input type={"number"} value={maxValue} onChange={changeMaxValue}/> max value
         <button onClick={set}>SET</button>
-        <h3 className={counter === 5 ? "red" : "counter"} >{counter}</h3>
-        <Button inc={incButtonHandler} reset={resetButtonHandler} counter={counter}/>
+        <h3 className={counter === maxValue ? "red" : "counter"} >{message ? message : counter}</h3>
+        <Button maxValue={maxValue} inc={incButtonHandler} reset={resetButtonHandler} counter={counter}/>
     </div>
   );
 }
@@ -33,21 +39,15 @@ type buttonType = {
     inc:()=>void
     reset:()=>void
     counter: number
+    maxValue: number
 
 }
 
 export const Button = (props: buttonType) => {
     return <div className="buttonIncReset">
-        <button disabled={props.counter >= 5} onClick={props.inc}>INC</button>
+        <button disabled={props.counter >= props.maxValue} onClick={props.inc}>INC</button>
         <button onClick={props.reset}>RESET</button>
     </div>
 }
-
-
-
-
-
-
-
 
 export default App;
